b695fa82ead5498ca7ef51fb1af11f7e952996de
nwabisadlokweni <dlonwa001@gmail.com>
2020-07-14 15:06:23 +0200

    changes

--- reg-number.dom.js
+++ reg-number.dom.js
@@ -3,13 +3,40 @@ const regNumber = document.querySelector(".regNumber");
 const regNumbers = document.querySelector(".regNumbers");
 const addRegNumberBtn = document.querySelector(".addRegNumber");
 const message = document.querySelector(".message");
+//const currentTown = document.querySelector(".message");
 
-addRegNumberBtn.addEventListener("click", function() {
+const regNumbersEntered = [];
+
+addRegNumberBtn.addEventListener("click", function () {
 
 	if (regNumberEntered.value == "") {
 		message.innerHTML = "Please enter a registration number";
 		return;
 	}
 
-	regNumber.innerHTML = regNumberEntered.value;
+	const currentRegNumber = regNumberEntered.value.toUpperCase();
+
+	if (currentRegNumber.startsWith("CY") ||
+		currentRegNumber.startsWith("CA") ||
+		currentRegNumber.startsWith("CJ")) {
+
+		message.innerHTML = "";
+
+		if (!regNumbersEntered.includes(currentRegNumber)) {
+
+			const regNumberElem = document.createElement("div");
+			regNumberElem.classList.add("regNumber");
+			regNumberElem.innerHTML = currentRegNumber;
+			regNumbers.appendChild(regNumberElem);
+
+
+			//store the reg number to the list
+			regNumbersEntered.push(currentRegNumber)
+
+	}else {
+		message.innerHTML = "This registarion number already exists.";
+	}
+}else {
+		message.innerHTML = "Invalid registration number entered.";
+	}
 });
